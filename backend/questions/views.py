6d3ec152fd87aab68aa92341bcb3953313b8460c
Иван Левков <ai.salome@yahoo.com>
2024-06-10 11:48:10 +0300

    now models is up to PEP8

--- backend/questions/views.py
+++ backend/questions/views.py
@@ -25,10 +25,11 @@ def get_question(question_id: int):
         }), 404
     return flask.jsonify({
         'error': False,
-        'question':
+        'question': {
             'id': question_id,
             'text': question.text,
             'comment': question.comment,
+        }
     }), 200
 
 @questions_router.route('/', methods=['GET'])
@@ -70,3 +71,37 @@ def get_questions():
             from_id=((page - 1) * page_size + 1), to_id=(page * page_size)
         )]
     }), 200
+
+@questions_router.route('/', methods=['POST'])
+def create_user():
+    '''Creates new question.
+    
+    Creates new question.
+
+    Args:
+        text (str): Text of the new question
+        comment (str): Comment to the question
+    '''
+
+    if flask.request.headers.get('Content-Type') != 'application/json':
+        return flask.jsonify({
+            'error': True,
+            'message': 'Incorrect Content-Type header',
+        })
+    text = flask.request.json.get('text', '')
+    comment = flask.request.json.get('text', '')
+
+    try:
+        question = models.Question.create_user(text, comment)
+    except ValueError as e:
+        return flask.jsonify({
+            'error': True,
+            'message': str(e),
+        }), 400
+    
+    return flask.jsonify({
+        'error': False,
+        'id': question.id,
+        'text': question.text,
+        'comment': question.comment,    
+    }), 201
