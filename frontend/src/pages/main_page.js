8df01fa337c10fe59fddd1b6dfd2111e3dfbacba
Minuta18 <ig.samsonov10@yandex.ru>
2024-06-11 17:10:32 +0300

    Main page done

--- frontend/src/pages/main_page.js
+++ frontend/src/pages/main_page.js
@@ -2,12 +2,26 @@ import React from 'react';
 
 import Background from '../ui/containers/background';
 import Modal from '../ui/containers/modal';
+import { 
+    LinkButtonPrimary, LinkButtonSecondary 
+} from '../ui/elements/buttons';
 
 export default function MainPage() {
     return (
         <>
             <Background>
-                <Modal><span className='header-text'>Hello world</span></Modal>
+                <Modal>
+                    <span className='header-text'>Добро пожаловать</span>
+                    <LinkButtonPrimary href='/auth/login'>
+                        Вход
+                    </LinkButtonPrimary>
+                    <LinkButtonPrimary href='/auth/register'>
+                        Регистрация
+                    </LinkButtonPrimary>
+                    <LinkButtonSecondary href='/play'>
+                        Играть без регистрации
+                    </LinkButtonSecondary>
+                </Modal>
             </Background>
         </>
     );
