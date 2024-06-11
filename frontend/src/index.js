03ac8d222973cc7ca66555c6fc9fb1ec794c9588
Minuta18 <ig.samsonov10@yandex.ru>
2024-06-11 16:04:23 +0300

    background

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import MainPage from './pages/main_page.js';
+
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainPage />,
+    }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={ router } />
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ frontend/src/pages/main_page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Background from '../ui/containers/background';
+
+export default function MainPage() {
+    return (
+        <>
+            <Background>
+                <h1>Hello world!</h1>
+            </Background>
+        </>
+    );
+}
