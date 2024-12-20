fb4f95746e0c612251c8a1c2d4333e09321dc3f1
Иван Левков <ai.salome@yahoo.com>
2024-12-20 13:47:48 +0300

    transition to postgres + minor additions

--- backend/game_sessions/models.py
+++ backend/game_sessions/models.py
@@ -1,5 +1,13 @@
 import api.endpoints
 import api.models
 from sqlalchemy import orm
+import sqlalchemy
 
 class GameSession(api.models.BaseModel):
+    '''Documentation'''
+
+    __tablename__ = 'game_sessions'
+
+    questions_order: orm.Mapped[str] = orm.mapped_column(
+        sqlalchemy.dialects.postgresql.ARRAY(sqlalchemy.Integer)
+    )
